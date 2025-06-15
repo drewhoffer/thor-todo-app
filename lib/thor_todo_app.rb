66c4903c36f486b66e3244612f4794dffbb518fb
fresh <drewsdhoffer@gmail.com>
2025-06-15 18:45:20 -0400

    init commit

---
+++ lib/thor_todo_app.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "thor_todo_app/version"
+
+module ThorTodoApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "thor_todo_app"
+
+require "minitest/autorun"
